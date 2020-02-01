9ae1d9469e6346bd481f345a1571fa89b45b6812
Nikita Kuprin <nikon5070@gmail.com>
2020-02-01 21:54:55 +1000

    init Storybook, add sass

---
+++ components/list/List.stories.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+// import Vue from 'vue'
+
+import { storiesOf } from '@storybook/vue'
+import List from './List'
+storiesOf('List', module)
+  .add('As a component', () => ({
+    components: { List },
+    template: '<List />'
+  }))
+  .add("I don't work", () => '<List />')
